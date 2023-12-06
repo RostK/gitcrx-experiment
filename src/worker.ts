2a3c58bede6eb9805a90e0d97aead934375de8a0
Rost Kaniuchenko <rkaniuchenko@gmail.com>
2023-12-06 18:22:31 +0000

    Step 3

--- src/worker.ts
+++ src/worker.ts
@@ -4,6 +4,16 @@ chrome.runtime.onInstalled.addListener(async () => {
     contexts: ["link"],
     title: "GitHub repo overview",
     type: "normal",
-    targetUrlPatterns: ["*://*.github.com/*"],
+    targetUrlPatterns: ["https://*.github.com/*"],
   });
+});
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "GET_GITHUB_INFO" && tab && info.linkUrl) {
+    void chrome.sidePanel.open({ tabId: tab?.id, windowId: tab?.windowId });
+    void chrome.sidePanel.setOptions({
+      tabId: tab?.id,
+      enabled: true,
+      path: `?url=${encodeURIComponent(info.linkUrl)}`,
+    });
+  }
 });
